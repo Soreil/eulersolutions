4a53768b5e32a2dc8451af9ebd31b10349ced3d7
Sjon Kosse <sjon@x61.eu>
2014-06-30 23:17:07 +0200

    problem 7 is getting there, logic done.

--- problem7/main.go
+++ problem7/main.go
@@ -1,14 +1,53 @@
 package main
+
+//Problem:
 //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 //What is the 10 001st prime number?
 import "fmt"
 import "os"
 import "strconv"
 
-func main() {
-	n, err := strconv.ParseInt(os.Args[1], 0, 0)
-	if err != nil {
-		panic(err)
+func sieve(n []int) []int {
+	number := 2
+	for number*number <= len(n)+2 {
+		for i := number + number; i < len(n)+2; i += number {
+			n[i-2] = 0
+		}
+		number++
+		for n[number-2] == 0 {
+			number++
+		}
 	}
-	fmt.Println(n)
+	return n
+}
+
+func fill(n []int) []int {
+	for i := 1; i <= len(n); i++ {
+		n[i-1] = i + 1
+	}
+	return n
+}
+
+func countprimes(n []int) int {
+	count := 0
+	for _, v := range n {
+		if v != 0 {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	var n int
+	if len(os.Args) == 2 {
+		n, _ = strconv.Atoi(os.Args[1])
+	} else {
+		panic("Enter a number")
+	}
+	list := make([]int, n)
+	fill(list)
+	sievedlist := sieve(list)
+	count := countprimes(sievedlist)
+	fmt.Println("amount of primes", count)
 }
