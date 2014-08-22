5c2a0e2010e02c7c4fd98483efb392f571061cc4
Sjon Kosse <sjon@x61.eu>
2014-08-22 13:54:03 +0200

    Forgot to check for integer overflow, the result couldn't fit in a int32 and was messing everything up, was mad with myself and just in64()'d the entire program because I had no interest in it anymore. Works just fine but it's mangled.

--- problem8/main.go
+++ problem8/main.go
@@ -26,47 +26,49 @@ package main
 
 //Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-)
+import "fmt"
 
-func adjacent_digits(s string, n int) int {
-	p := 0
-	l := len(s) - n - 1
-	fmt.Println(l)
+var digits string = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
 
-	for i := 0; i < l-n; i++ {
-		sub := s[l : l+n]
-		var total int
-		var j int
-		for j, total = 1, sub[0]; j < n; j++ {
-			total *= sub[j]
+func biggestProduct(n int64) (int64, []int64) {
+	numbers := make([]int64, n)
+	//biggestSum := int64(0)
+	biggestProduct := int64(0)
+	var tmpNumbers []int64
+
+	for i := int64(0); i < int64(len(digits))-n; i++ {
+
+		tmpNumbers, _ = adjacentDigits(i, n)
+
+		//if tmpBiggestSum > biggestSum {
+		if product(tmpNumbers) > product(numbers) {
+			numbers = tmpNumbers
+			//biggestSum = tmpBiggestSum
+			biggestProduct = product(numbers)
 		}
-		if total > p {
-			p = total
-		}
+		//}
 	}
-	return p
+	return biggestProduct, numbers
+}
+
+func product(numbers []int64) int64 {
+	product := int64(1)
+	for i := range numbers {
+		product *= numbers[i]
+	}
+	return product
+}
+
+func adjacentDigits(idx int64, n int64) ([]int64, int64) {
+	numbers := make([]int64, n)
+	sum := int64(0)
+	for i := int64(0); i < n; i++ {
+		numbers[i] = int64(digits[idx+i]) - '0'
+		sum += int64(digits[idx+i]) - '0'
+	}
+	return numbers, sum
 }
 
 func main() {
-	var filename string
-	var adjdigits int
-	if len(os.Args) == 3 {
-		filename = os.Args[1]
-		adjdigits, _ = strconv.Atoi(os.Args[2])
-	} else {
-		panic("provide a filename and amount of adjacent digits")
-	}
-
-	fi, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	s := string(fi)
-	r := adjacent_digits(s, adjdigits)
-	fmt.Println(r, adjdigits)
+	fmt.Println(biggestProduct(13))
 }
