1bde42dd9f53aafbf713316830713ceee813ec7f
Sjon Kosse <sjon@x61.eu>
2014-08-29 13:42:12 +0200

    Problem 12 done. At least now it doesn't take forever to run, really managed to simplify it a lot. Count is still wrong (~2x what it should be)

--- problem12/main.go
+++ problem12/main.go
@@ -9,18 +9,21 @@ import (
 
 var primes = [...]int64{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241}
 
-func factors(n int64, c chan int64) {
+// somewhere in this function there is a mistake, I get about double the count I should be getting.
+// Probably shouldn't count past the middle(middle of what?) and have some bounds errors.
+func factors(n int64) int64 {
 	count := int64(0)
+	sqrt := int64(math.Sqrt(float64(n)))
 	for i := range primes {
-		if n%primes[i] == 0 && primes[i] < int64(math.Sqrt(float64(n))) {
-			for j := int64(1); j <= 500/primes[i]; j++ {
+		if n%primes[i] == 0 && primes[i] < sqrt {
+			for j := int64(1); j < sqrt/primes[i]; j++ {
 				if n%(primes[i]*j) == 0 {
 					count++
 				}
 			}
 		}
 	}
-	c <- count
+	return count
 }
 
 func main() {
@@ -29,25 +32,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	count := int64(0)
+	ret := int64(0)
 	trianglenumber := int64(0)
-	c := make(chan int64)
-	done := make(chan bool)
-	go func() {
-		tmp := int64(0)
-		count := int64(0)
-		for count < n {
-			if tmp = <-c; tmp > count {
-				count = tmp
-				fmt.Println(count)
-				if count > 500 {
-					done <- true
-				}
-			}
-		}
-	}()
 	for i := int64(1); i <= n; i++ {
 		trianglenumber += i
-		go factors(trianglenumber, c)
+		if ret = factors(trianglenumber); ret > count {
+			count = ret
+			fmt.Println(count)
+			if count > 500 {
+				fmt.Println("result found,", trianglenumber)
+			}
+		}
 	}
-	<-done
 }
