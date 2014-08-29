7ba38e1993761c038c8a1829ebf4b01db5ccd9bb
Sjon Kosse <sjon@x61.eu>
2014-08-29 17:19:58 +0200

    Problem 14 done Very easy problem, can be made fancier but this is sufficiently fast. Nice to see recursion working out.

---
+++ problem14/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func collatz(n, count int) int {
+	count++
+	if n%2 == 0 {
+		return collatz(n/2, count)
+	} else if n != 1 {
+		return collatz((3*n)+1, count)
+	}
+	return count
+}
+func main() {
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	biggest := 0
+	tmp := 0
+	for i := 1; i < n; i++ {
+		tmp = collatz(i, 0)
+		if tmp > biggest {
+			biggest = tmp
+			fmt.Println(biggest, i)
+		}
+	}
+}
