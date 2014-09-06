c706238b8aeedaeec4732415503bf052068a96e5
Sjon Kosse <sjon@x61.eu>
2014-09-06 16:08:14 +0200

    useless parens.

--- problem28/main.go
+++ problem28/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func spiralDiagonals(n int64) (sum int64) {
 	sum = 1
 	for n > 1 {
-		sum += n*n*4 - ((n - 1) * 6) // The 6 is 0+1+2+3
+		sum += n*n*4 - (n-1)*6 // The 6 is 0+1+2+3
 		n -= 2
 	}
 	return
