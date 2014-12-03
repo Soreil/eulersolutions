fcf5f5ded836aa88f0b802c22c1ac870f0c13499
Sjon Kosse <sjon@x61.eu>
2014-12-03 17:24:12 +0100

    Problem 27 done. More opimisation is possible, runtime is fine atm

--- problem27/main.go
+++ problem27/main.go
@@ -2,39 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/Soreil/eulersolutions/utils"
 )
 
-var alim int = 1000
-var blim int = 1000
-
-var primes []int = utils.Sieve(100000)
+var primes []int = utils.Sieve(10000)
 
 func main() {
-	a, b := -999, -999
-	// test values that produce 40 primes, go above 1000
-	a = 1
-	b = 41
-	length, longest := 0, 0
+	var alim int = 1000
+	var blim int = 1000
+	longest := 0
 	//Figure out a logical path for decrementing a and b
-	//for math.Abs(float64(a)) < float64(alim) && math.Abs(float64(b)) < float64(blim) {
-	length = 0
-	//no idea how big n will get, shouldn't need a limit
-	for n := 0; n < 1000; n++ {
-		if isPrime(genPrime(a, b, n)) {
-			length++
-		} else {
-			fmt.Println("Not a winner", a, b, n)
-			fmt.Println(genPrime(a, b, n))
-			break
+	for a := -1000; math.Abs(float64(a)) <= float64(alim); a++ {
+		for b := 0; b < blim; b++ {
+			n := 0
+			for {
+				if isPrime(genPrime(a, b, n)) {
+					n++
+				} else {
+					break
+				}
+			}
+			if n > longest {
+				longest = n
+				fmt.Println(a, b, n)
+			}
 		}
 	}
-	if length > longest {
-		longest = length
-		fmt.Println(longest)
-	}
-	//}
 }
 
 func genPrime(a, b, n int) int {
