ff7c79e8e459dc33117392f10724163a36f36172
Sjon Kosse <sjon@x61.eu>
2015-01-12 15:08:26 +0100

    Some comments for a change

--- problem34/main.go
+++ problem34/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	//The number 9.999.999 is the largest amount of 9s
-	//we can have before the result is smaller than the original number
+	//we can have before the result is ALWAYS smaller than the original number
+	//Make a slice with the factorials so we don't duplicate computation
 	ffact := make([]int, 10)
 	for i := range ffact {
 		ffact[i] = utils.Factorial(i)
@@ -16,13 +17,13 @@ func main() {
 	lim := ffact[9] * 7
 	var sum int
 	var tmpSum int
+	//Do not count 1 and 2
 	for i := 3; i < lim; i++ {
 		tmpSum = 0
 		for n := i; n > 0; n /= 10 {
 			tmpSum += ffact[n%10]
 		}
 		if tmpSum == i {
-			fmt.Println(tmpSum)
 			sum += tmpSum
 		}
 	}
