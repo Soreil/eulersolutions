bdd8e2dea2205f55c32049328709602dd0c099df
Sjon Kosse <sjon@x61.eu>
2015-03-09 00:13:36 +0100

    I had to play around with recursion depth limits to get it to run in a decent timeframe. I should proper figure out how to do it more cleanly.

---
+++ problem50/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Soreil/eulersolutions/utils"
+)
+
+var primes []int
+
+func main() {
+	primes = utils.Sieve(1000000)
+
+	var sum []int
+	var prime int
+
+	for _, v := range primes {
+		if tmpSum := consPrimeSum(v, 0); len(tmpSum) > len(sum) {
+			sum = tmpSum
+			prime = v
+		}
+	}
+	fmt.Println(sum)
+	fmt.Println("Prime:", prime)
+}
+
+func consPrimeSum(p int, count int) []int {
+	if count > 100 {
+		return []int{}
+	}
+	fmt.Println(p, count)
+	var sum int
+	for i := range primes {
+		if i+count >= len(primes) {
+			break
+		}
+		sum += primes[i+count]
+		if sum > p {
+			return consPrimeSum(p, count+1)
+		} else if sum == p {
+			return primes[count : i+count+1]
+		}
+	}
+	return []int{}
+}
