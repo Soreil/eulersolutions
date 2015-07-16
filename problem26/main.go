335bdc6ae34e0ba1ffa618732aeb0ba7b58620c4
Sjon Kosse <sjon@x61.eu>
2015-07-17 01:12:28 +0200

    Finished problem 26.
    Interesting to find this was doable with long division, something I had
    long since forgotten about.

--- problem26/main.go
+++ problem26/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func coprime10(n int64) bool {
+	"github.com/Soreil/eulersolutions/utils"
+)
+
+func coprime10(n int) bool {
 	for n%2 == 0 {
 		n /= 2
 	}
@@ -15,41 +20,58 @@ func coprime10(n int64) bool {
 	return false
 }
 
-//Calculates the greatest commmon divisor of two numbers
-func gcd(x, y int64) int64 {
-	var min int64
-	if x < y {
-		min = x
-	} else {
-		min = y
-	}
-	for i := min; i > 1; i-- {
-		if x%i == 0 && y%i == 0 {
-			return i
-		}
-	}
-	return 1
+//
+////Calculates the greatest commmon divisor of two numbers
+//func gcd(x, y int64) int64 {
+//	var min int64
+//	if x < y {
+//		min = x
+//	} else {
+//		min = y
+//	}
+//	for i := min; i > 1; i-- {
+//		if x%i == 0 && y%i == 0 {
+//			return i
+//		}
+//	}
+//	return 1
+//
+//}
 
+func isCyclic(p int) bool {
+	b := 10
+	t := 0
+	r := 1
+	n := 0
+	for {
+		t++
+		x := r * b
+		d := x / p
+		r = x % p
+		n = n*b + d
+		if r == 1 {
+			break
+		}
+		log.Println(n)
+	}
+	return t == p-1
 }
 
 func main() {
-
-	var lim int64 = 1000
-
-	for d := int64(2); d < lim; d++ {
+	var lim int = 1000
+	primes := utils.Sieve(lim)
+	var max int
+	for d := 2; d <= lim; d++ {
 		//Coprimes of 10(multiples of 2 and 5) can't be cyclic
 		if coprime10(d) {
 			continue
 		}
-		var i int64
-		var tmp int64
-		for tmp, i = d, 1; gcd(tmp, 10) != 1; i++ {
-			tmp *= d
-		}
-		if i == 1 {
-			fmt.Println(d, " ", d-1)
-		} else {
-			fmt.Println(d, " ", i-1, "times something")
+		if utils.IsPrime(d, primes) {
+			//	fmt.Println(d, isCyclic(d))
+			if isCyclic(d) {
+				max = d
+			}
 		}
 	}
+	fmt.Println("Result:", max)
 }
